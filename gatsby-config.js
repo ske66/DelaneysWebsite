2b02895e22028373ff4c1c349f073334f5965688
Mark Barton <40204953@live.napier.ac.uk>
2021-01-12 21:35:05 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "Delaneys Site",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "e0_uGbjhV6l_wKLag_a3RoqW_d4esTJ8_kkgQ1PRGJU",
+        spaceId: "",
+      },
+    },
+  ],
+};
